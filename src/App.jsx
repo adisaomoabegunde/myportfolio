d72397fa932c7a693066a82954fef615df8532c6
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2026-01-03 14:57:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
+import { Route } from "react-router-dom"
+import { Routes } from "react-router-dom"
+import { Toaster } from "./components/ui/toaster"
+
+function App() {
+
+
+  return (
+    <>
+    <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillSection } from "../components/SkillsSection";
+import { ProjectSection } from "../components/ProjectSection";
+import { ContactSection } from "../components/ContactSection";
+import { FooterSection } from "../components/FooterSection";
+
+export const Home= () =>{
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Theme Toggle */}
+        <ThemeToggle />
+
+        {/* Background Effects */}
+        <StarBackground />
+
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Main Content */}
+        <main>
+            <HeroSection />
+            <AboutSection />
+            <SkillSection />
+            <ProjectSection />
+            <ContactSection />
+        </main>
+        <FooterSection />
+        {/* Footer */}
+    </div>;
+}
