afb49bed6b5773ee656699594a3d25b6716daba0
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2026-01-08 15:11:58 +0100

    Fixxxxxxxxxxxxxxxxx

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -64,7 +64,7 @@ const projects = [
 ]
 
 export const ProjectSection = () => {
-    return <section id="projects" className="py-24 px-4 relative">
+    return <section id="projects" className="py-24 px-4 relative z-10">
         <div className="container mx-auto max-w-5xl">
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Featured <span className="text-primary">Projects</span></h2>
 
